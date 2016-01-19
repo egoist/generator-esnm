89cb192b8b4e9956117ac9cbc54f903bc33168c8
EGOIST <0x142857@gmail.com>
2016-01-19 13:34:09 +0800

    strip semi

--- app/templates/cli.js
+++ app/templates/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-'use strict';
-var meow = require('meow');
-var <%= camelModuleName %> = require('./');
+'use strict'
+var meow = require('meow')
+var <%= camelModuleName %> = require('./')
 
 var cli = meow([
 	'Usage',
@@ -15,6 +15,6 @@ var cli = meow([
 	'  unicorns & rainbows',
 	'  $ <%= moduleName %> ponies',
 	'  ponies & rainbows'
-]);
+])
 
-console.log(<%= camelModuleName %>(cli.input[0] || 'unicorns'));
+console.log(<%= camelModuleName %>(cli.input[0] || 'unicorns'))
